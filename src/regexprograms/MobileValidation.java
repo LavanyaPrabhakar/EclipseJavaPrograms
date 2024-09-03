1097e3f2c445376f616d70c8be0fbac7fd38951c
LavanyaPrabhakar <Lavanya.Prabhakar@gmail.com>
2024-09-03 09:57:19 +0100

    Added MobileValidation program to regexprograms package

---
+++ src/regexprograms/MobileValidation.java
@@ -0,0 +1,35 @@
+package regexprograms;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileValidation {
+
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		File file = new File("C:\\Users\\LPrab\\OneDrive\\Documents\\Lavanya_Tutorial\\New Tutorial\\JavaLesson\\SampleFile.txt");
+		Scanner scan = new Scanner(file);
+			
+		while(scan.hasNext())
+		{
+			String line = scan.nextLine();
+			String[] parts = line.split(":");
+			String mobile = parts[1];
+            
+            
+            Pattern p = Pattern.compile("^(\\+44\\s?7\\d{3}|\\(?07\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}$");
+            Matcher m = p.matcher(mobile);
+            
+            if(m.matches())
+            { System.out.println(mobile + " is a valid UK mobile number");}
+            else 
+            { System.out.println(mobile + " is not a valid UK mobile number");}         
+         }
+		scan.close();
+        }	      
+	}
+
+
