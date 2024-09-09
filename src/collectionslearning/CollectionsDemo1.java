ebeede11cb577a7d148ea38aa4336f580b9713cd
LavanyaPrabhakar <Lavanya.Prabhakar@gmail.com>
2024-09-09 07:29:35 +0100

    CollectionsDemo1 class added in Collectionslearning

---
+++ src/collectionslearning/CollectionsDemo1.java
@@ -0,0 +1,21 @@
+package collectionslearning;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionsDemo1 {
+
+	public static void main(String[] args) {
+	    ArrayList<Integer> numbers = new ArrayList<Integer>();
+	    numbers.add(12);
+	    numbers.add(8);
+	    numbers.add(2);
+	    numbers.add(23);
+	    System.out.println("Before sorting :"+numbers);
+	    Collections.sort(numbers);
+	    System.out.println("After sorting :"+numbers);
+	    
+	    System.out.println("Index value of key is :"+Collections.binarySearch(numbers, 23));
+	}
+
+}
