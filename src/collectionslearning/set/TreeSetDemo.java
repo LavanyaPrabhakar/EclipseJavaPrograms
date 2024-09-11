e3ba6217b156549563b17a165ce5039996272e49
LavanyaPrabhakar <Lavanya.Prabhakar@gmail.com>
2024-09-11 13:45:06 +0100

    Added RemoveDupSortOrder to collectionslearning package

--- src/collectionslearning/set/TreeSetDemo.java
+++ src/collectionslearning/set/TreeSetDemo.java
@@ -1,8 +1,10 @@
 /*SortedTreeSet:
  * No duplicates allowed
- *  sorting order: Natural/Comparator Specified will be maintained
- * 
- * 
+ * sorting order: Natural/Comparator Specified will be maintained
+ * Since sorting occurs- it will allow homogenous object 
+ * can't include : sts.add('c'); to the below sts TreeSet as it has number type objects
+ * When used will throw ClassCastException
+ * When null value is added: sts.add(null);- will throw NullPointerException
  */
 
 package collectionslearning.set;
@@ -19,8 +21,9 @@ public class TreeSetDemo {
 			sts.add(10);
 			sts.add(200);
 			sts.add(50);
-			sts.add(70);
+			sts.add(70);			
 			sts.add(100);
+			//can't include : sts.add('c'); When used will throw ClassCastException
 			System.out.println("Check for Duplication allowed?  :"+sts.add(100));
 			System.out.println("Natural(Ascending) sorting order without duplicates :");
 			//dupe 100 not allowed but displayed insertion order
